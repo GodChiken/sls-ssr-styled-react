aaa9bd81577e6eab90700d23d1435e7986b7d495
twiw49 <twiw49@gmail.com>
2018-05-09 01:07:12 +0900

    for deploy

--- src/lambda.js
+++ src/lambda.js
@@ -1,6 +1,15 @@
 const awsServerlessExpress = require("aws-serverless-express");
 const app = require("./server");
-const server = awsServerlessExpress.createServer(app);
+const binaryMimeTypes = [
+  "application/octet-stream",
+  "font/eot",
+  "font/opentype",
+  "font/otf",
+  "image/jpeg",
+  "image/png",
+  "image/svg+xml"
+];
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 
 exports.handler = (event, context) => {
   console.log(event);
