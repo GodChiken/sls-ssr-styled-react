442e263c4b7bd3f92239758d836ad7d19a009cd3
twiw49 <twiw49@gmail.com>
2018-05-09 00:52:19 +0900

    for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const clientConfig = {
   entry: "./src/browser.jsx",
   output: {
     path: path.join(__dirname, "dist/public"),
-    publicPath: "/dist/public",
+    publicPath: "/dist/public/",
     filename: "bundle.[hash:6].js"
   },
   module: {
