8b5719d702fd7582924e5400b3685844a6df5d4f
twiw49 <twiw49@gmail.com>
2018-05-09 03:02:04 +0900

    for deploy

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -5,22 +5,22 @@ import Home from "./Home";
 import LandingPage from "./LandingPage";
 import { connect } from "react-redux";
 
-console.log(__dirname);
+console.log(process.env.NODE_ENV);
 
 const App = ({ isLoggedIn }) => {
   if (isLoggedIn) {
     return (
       <Switch>
-        <Route exact path={__dirname} component={Home} />
-        <Redirect to={__dirname} />
+        <Route exact path="/dev" component={Home} />
+        <Redirect to="/dev" />
       </Switch>
     );
   }
 
   return (
     <Switch>
-      <Route exact path={__dirname} component={LandingPage} />
-      <Redirect to={__dirname} />
+      <Route exact path="/dev" component={LandingPage} />
+      <Redirect to="/dev" />
     </Switch>
   );
 };
