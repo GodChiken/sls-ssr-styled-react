5944ad510f83acbfd81c95b75c215a0ff472fa2e
twiw49 <twiw49@gmail.com>
2018-05-09 09:26:22 +0900

    for deploy

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -9,7 +9,7 @@ const App = ({ isLoggedIn }) => {
   if (isLoggedIn) {
     return (
       <Switch>
-        <Route exact path="/dev" component={Home} />
+        <Route exact url="/dev" component={Home} />
         <Redirect to="/dev" />
       </Switch>
     );
@@ -17,7 +17,7 @@ const App = ({ isLoggedIn }) => {
 
   return (
     <Switch>
-      <Route exact path="/dev" component={LandingPage} />
+      <Route exact url="/dev" component={LandingPage} />
       <Redirect to="/dev" />
     </Switch>
   );
